89c7ba0e20a2042a9ebcb01240d7b9c2aa6367f6
Jonathan Rojas <jonathan.rojas34@hotmail.com>
2016-11-18 22:21:32 -0500

    Implemented <Product> object with ArrayList

--- DatabaseHelper.java
+++ DatabaseHelper.java
@@ -1,52 +1,16 @@
+//NOTE: IN THE 'url' VARIABLE, BE SURE TO CHANGE VALUE FROM 'POWERCORE' TO WHATEVER YOUR SERVER INSTANCE IS NAMED!
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.HashSet;
 
 @SuppressWarnings("unused")
 public class DatabaseHelper extends Product{
-	public static void main(String[] args){
-		String url = "jdbc:sqlserver://POWERCORE:1433;databaseName=ecommerce;integratedSecurity=true";
-		//connectToDB(url);
-		populateListProduct(url);
-	}
-
-	public static String[] populateListProduct(String url) {
-		url = "jdbc:sqlserver://POWERCORE:1433;databaseName=ecommerce;integratedSecurity=true";
-		String[] uniqueResults = null;
-		Connection c;
-		/**/
-		try {
-			c = DriverManager.getConnection(url);
-			String s = "SELECT * FROM Products "; /**/
-			PreparedStatement statement = c.prepareStatement(s); /**/ /**/
-			ResultSet rs = statement.executeQuery();
-			int g = 0;
-			while (rs.next()) {
-				String[] results = {rs.getString("Serial_Number"),
-						rs.getString("Product_Name"),
-						rs.getString("Gender"),
-						rs.getString("Price"),
-						rs.getString("Category"),
-						rs.getString("SubCategory"),
-						rs.getString("Product_Description"),
-						rs.getString("Manufacturer"),
-						rs.getString("Supplier")
-				};
-				uniqueResults = Arrays.stream(results).distinct().toArray(String[]::new);
-				System.out.println(Arrays.toString(uniqueResults));
-			}
-			rs.close();
-		} catch (Exception e) {
-			System.out.println(e);
-		}
-		return uniqueResults;
-	}
-
 	public static void connectToDB(String url) {
 		try{
 			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
@@ -58,40 +22,51 @@ public class DatabaseHelper extends Product{
 			System.out.println("Connecting.....");
 			Connection conn = DriverManager.getConnection(url);
 			System.out.println("-----Connected." );
-			/*			conn.close();
-			System.out.println("-----Connection closed" );*/
 		} catch (Exception ex) {
 			System.out.println("Error Connecting: "+ex.getMessage());
 		}
 	}
-}
 
-//This is servlet prelimiary code, just ignore this stuff for now...
-
-/*import javax.servlet.http.*;  
-import javax.servlet.*;  
-import java.io.*;
-import java.util.ArrayList;  
-public class DemoServlet extends HttpServlet{  
-	public void doGet(HttpServletRequest req,HttpServletResponse res)  
-			throws ServletException,IOException  
-	{  
-		res.setContentType("text/html");//setting the content type  
-		PrintWriter pw=res.getWriter();//get the stream to write the data  
-		String url = "jdbc:sqlserver://Jonathan-Server:1433;databaseName=ecommerce;integratedSecurity=true";
-		//writing html in the stream  
-		pw.println("<html><body>");  
-		ArrayList<Product> myProduct = DatabaseHelper.populateListProduct(url);
-		for (Product d:myProduct){
-			for(int i = 0; i < myProduct.size(); i++){
-				pw.print(myProduct.get(i).getProductName() +"\t");
-				pw.println(myProduct.get(i).getSerialNumber());
-				//System.out.println(products.get(i));
+	public static ArrayList<Product> populateListProduct(String url) {
+		url = "jdbc:sqlserver://POWERCORE:1433;databaseName=ecommerce;integratedSecurity=true";
+		ArrayList<Product> products = new ArrayList<Product>();
+		Product product = null;
+		Connection c;
+		try {
+			c = DriverManager.getConnection(url);
+			String s = "SELECT * FROM Products"; 
+			PreparedStatement statement = c.prepareStatement(s); 
+			ResultSet rs = statement.executeQuery();
+			int g =0;
+			while (rs.next()) {
+					product = new Product();
+					product.setSerialNumber(rs.getString("Serial_Number"));
+					product.setProductName(rs.getString("Product_Name"));
+					product.setGender(rs.getString("Gender"));
+					product.setPrice(rs.getDouble("Price"));
+					product.setCategory(rs.getString("Category"));
+					product.setSubCategory(rs.getString("SubCategory"));
+					product.setProductDescription(rs.getString("Product_Description"));
+					product.setManufacturer(rs.getString("Manufacturer"));
+					product.setSupplier(rs.getString("Supplier"));	
+					products.add(product);
 			}
+			rs.close();
+			int i = 1;
+			for (Product d:products){
+					System.out.print(i+ "> "+d.getProductName() +"\t");
+					System.out.println(d.getSerialNumber());
+					i++;
+			}
+		} catch (Exception e) {
+			System.out.println(e);
 		}
-		pw.println("Welcome to servlet");  
-		pw.println("</body></html>");  
-
-		pw.close();//closing the stream  
+		return products;
 	}
-}*/
+
+	public static void main(String[] args){
+		String url = "jdbc:sqlserver://POWERCORE:1433;databaseName=ecommerce;integratedSecurity=true";
+		//connectToDB(url);
+		populateListProduct(url);
+	}
+}
